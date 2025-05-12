6ea7333e6dc036051ff6b383d263adbeeab50d27
InkedCat <146424693+InkedCat@users.noreply.github.com>
2025-05-12 02:08:34 +0200

    feat(deep-link): integrate deep link functionality and update dependencies

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,8 @@
+mod auth;
 mod tray_icon;
 
-use tauri::{Window, WindowEvent};
+use tauri::{AppHandle, Manager, Window, WindowEvent};
+use tauri_plugin_deep_link::DeepLinkExt;
 
 pub static MAIN_WINDOW_LABEL: &str = "main";
 
@@ -16,13 +18,37 @@ fn handle_window_event(window: &Window, event: &WindowEvent) {
     }
 }
 
+async fn setup(app: &AppHandle) -> Result<(), tauri_plugin_deep_link::Error> {
+    #[cfg(any(windows, target_os = "linux"))]
+    {
+        app.deep_link().register_all()?;
+    }
+    app.deep_link().on_open_url(|event| {
+        println!("Open URL: {:?}", event.urls());
+    });
+    Ok(())
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
-        .on_window_event(handle_window_event)
+    let mut builder = tauri::Builder::default();
+
+    #[cfg(desktop)]
+    {
+        builder = builder.plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = app.get_webview_window("main")
+                       .expect("no main window")
+                       .set_focus();
+        }));
+    }
+
+    builder
+        .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_opener::init())
+        .on_window_event(handle_window_event)
         .invoke_handler(tauri::generate_handler![])
         .setup(|app| {
+            tauri::async_runtime::block_on(setup(app.handle()))?;
             tray_icon::enable_tray(app)?;
             Ok(())
         })
