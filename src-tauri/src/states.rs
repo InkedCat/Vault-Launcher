b7d34229227be7d7770212be930357d9b0cfc903
InkedCat <146424693+InkedCat@users.noreply.github.com>
2025-05-26 00:13:35 +0200

    feat(auth): implement state management for authentication data

---
+++ src-tauri/src/states.rs
@@ -0,0 +1,18 @@
+use std::sync::Mutex;
+
+use tauri::Manager;
+
+#[derive(Debug, Clone)]
+pub struct AuthData {
+    pub state: Option<String>,
+    pub code_verifier: Option<String>,
+}
+
+pub fn create_states(app_handle: &tauri::AppHandle) -> () {
+    let auth_data = Mutex::new(AuthData {
+        state: None,
+        code_verifier: None,
+    });
+
+    app_handle.manage(auth_data);
+}
