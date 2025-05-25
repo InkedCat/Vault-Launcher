255c550170a4d06f18029ef28d3961bcdadd5199
InkedCat <146424693+InkedCat@users.noreply.github.com>
2025-05-26 00:09:36 +0200

    feat(account): implement deep link handling for Microsoft OAuth

---
+++ src-tauri/src/account.rs
@@ -0,0 +1,22 @@
+pub mod auth;
+pub mod login;
+
+const MICROSOFT_OAUTH_API_URL: &str = "https://login.microsoftonline.com/consumers/oauth2/v2.0";
+const XBOX_LIVE_URL: &str = "https://user.auth.xboxlive.com/";
+
+const MICROSOFT_CLIENT_ID: &str = env!("MICROSOFT_CLIENT_ID", "MICROSOFT_CLIENT_ID is not defined");
+const REDIRECT_URI: &str = "vault-launcher://account/login/callback";
+
+fn handle_login_callback(app_handle: &tauri::AppHandle, url: &tauri::Url) {
+    match url.query() {
+        Some(query) => {}
+        None => log::error!("No query parameters found"),
+    }
+}
+
+pub fn handle_deep_link(app_handle: &tauri::AppHandle, url: tauri::Url) {
+    match url.path() {
+        "/login/callback" => handle_login_callback(app_handle, &url),
+        _ => log::error!("Path not found"),
+    }
+}
--- src-tauri/src/tray_icon.rs
+++ src-tauri/src/tray_icon.rs
@@ -3,55 +3,60 @@ use tauri::{
     tray::{TrayIcon, TrayIconBuilder, TrayIconEvent},
     AppHandle, Manager, WebviewWindow,
 };
+use thiserror::Error;
 
 use crate::MAIN_WINDOW_LABEL;
+
+#[derive(Error, Debug)]
+pub enum TrayIconSetupError {
+    #[error("failed to create tray icon: {0}")]
+    CreateTrayIconError(#[from] tauri::Error),
+}
 
 fn menu_handler(app: &AppHandle, event: MenuEvent) {
     match event.id().as_ref() {
         "quit" => {
-            println!("Quit menu item clicked");
             app.exit(0);
         }
         _ => {
-            println!("Menu item {:?} not handled", event.id);
+            log::warn!("unhandled menu item {:?} called", event.id);
+        }
+    }
+}
+fn show_window(window: WebviewWindow) -> () {
+    match window.is_visible() {
+        Ok(true) => (),
+        Ok(false) => {
+            if let Err(e) = window.show() {
+                log::error!("failed to show {MAIN_WINDOW_LABEL} window: {:?}", e);
+            }
+
+            if let Err(e) = window.set_focus() {
+                log::error!("failed to focus {MAIN_WINDOW_LABEL} window: {:?}", e);
+            }
+        }
+        Err(e) => {
+            log::error!("failed to check if window is visible: {:?}", e);
         }
     }
 }
 
 fn tray_icon_handler(icon: &TrayIcon, event: TrayIconEvent) -> () {
-    fn toggle_window(window: WebviewWindow) -> () {
-        match window.is_visible() {
-            Ok(true) => {}
-            Ok(false) => {
-                if let Err(e) = window.show() {
-                    eprintln!("Failed to show window: {:?}", e);
-                }
-
-                if let Err(e) = window.set_focus() {
-                    eprintln!("Failed to focus window: {:?}", e);
-                }
-            }
-            Err(e) => {
-                eprintln!("Failed to check if window is visible: {:?}", e);
-            }
-        }
-    }
-
     match event {
         TrayIconEvent::Click { .. } => {
             if let Some(window) = icon.app_handle().get_webview_window(MAIN_WINDOW_LABEL) {
-                toggle_window(window);
+                show_window(window);
             } else {
-                eprintln!("Failed to get window");
+                log::error!("failed to retrieve {MAIN_WINDOW_LABEL} window");
             }
         }
         _ => (),
     }
 }
 
-pub fn enable_tray(app: &mut tauri::App) -> tauri::Result<()> {
+pub fn setup(app: &mut tauri::App) -> Result<(), TrayIconSetupError> {
     let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
-    let menu = MenuBuilder::new(app).item(&quit_i).build().unwrap();
+    let menu = MenuBuilder::new(app).item(&quit_i).build()?;
 
     let _ = TrayIconBuilder::new()
         .icon(app.default_window_icon().unwrap().clone())
@@ -61,5 +66,5 @@ pub fn enable_tray(app: &mut tauri::App) -> tauri::Result<()> {
         .on_tray_icon_event(tray_icon_handler)
         .build(app)?;
 
-    return Ok(());
+    Ok(())
 }
