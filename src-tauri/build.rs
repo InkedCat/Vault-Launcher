0d3668a9ce2be8f0f920dc19c13ac55d3dcb0025
InkedCat <146424693+InkedCat@users.noreply.github.com>
2025-05-25 20:13:26 +0200

    chore(build): add validation for MICROSOFT_CLIENT_ID environment variable

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,14 @@
 fn main() {
+    let microsoft_client_id = match std::env::var("MICROSOFT_CLIENT_ID") {
+        Ok(id) => id,
+        Err(_) => {
+            panic!("MICROSOFT_CLIENT_ID is not defined");
+        }
+    };
+
+    if microsoft_client_id.is_empty() {
+        panic!("MICROSOFT_CLIENT_ID is not defined");
+    }
+
     tauri_build::build()
 }
