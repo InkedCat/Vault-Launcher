f85251f34607cec19ed1324b04e97bc19949f04a
InkedCat <146424693+InkedCat@users.noreply.github.com>
2025-05-30 15:03:21 +0200

    refactor(renderer): clean up and format code in skin-viewer

--- src/lib/components/skin-viewer/renderer.svelte
+++ src/lib/components/skin-viewer/renderer.svelte
@@ -1,56 +1,49 @@
 <script lang="ts">
-    import { useThrelte, useTask } from '@threlte/core'
-    import { onMount } from 'svelte'
-    import {
-      EffectComposer,
-      EffectPass,
-      RenderPass,
-	  ShaderPass,
-	  SMAAEffect,
-	  SMAAPreset,
-    } from 'postprocessing'
+	import { useThrelte, useTask } from '@threlte/core';
+	import { onMount } from 'svelte';
+	import { EffectComposer, EffectPass, RenderPass, SMAAEffect, SMAAPreset } from 'postprocessing';
 	import { Camera } from 'three';
 
-    const { scene, renderer, camera, size } = useThrelte()
-  
-    // Adapt the default WebGLRenderer: https://github.com/pmndrs/postprocessing#usage
-    const composer = new EffectComposer(renderer)
-  
-    const setupEffectComposer = (camera: Camera) => {
-      composer.removeAllPasses()
-      composer.addPass(new RenderPass(scene, camera))
-      composer.addPass(
-      new EffectPass(
-        camera,
-        new SMAAEffect({
-          preset: SMAAPreset.LOW
-        })
-      )
-    )
-    }
-  
-    // We need to set up the passes according to the camera in use
-    $effect(() => {
-      setupEffectComposer($camera)
-    })
-    $effect(() => {
-      composer.setSize($size.width, $size.height)
-    })
-  
-    const { renderStage, autoRender } = useThrelte()
-  
-    // We need to disable auto rendering as soon as this component is
-    // mounted and restore the previous state when it is unmounted.
-    onMount(() => {
-      let before = autoRender.current
-      autoRender.set(false)
-      return () => autoRender.set(before)
-    })
-  
-    useTask(
-      (delta) => {
-        composer.render(delta)
-      },
-      { stage: renderStage, autoInvalidate: false }
-    )
-  </script>
+	const { scene, renderer, camera, size } = useThrelte();
+
+	// Adapt the default WebGLRenderer: https://github.com/pmndrs/postprocessing#usage
+	const composer = new EffectComposer(renderer);
+
+	const setupEffectComposer = (camera: Camera) => {
+		composer.removeAllPasses();
+		composer.addPass(new RenderPass(scene, camera));
+		composer.addPass(
+			new EffectPass(
+				camera,
+				new SMAAEffect({
+					preset: SMAAPreset.LOW
+				})
+			)
+		);
+	};
+
+	// We need to set up the passes according to the camera in use
+	$effect(() => {
+		setupEffectComposer($camera);
+	});
+	$effect(() => {
+		composer.setSize($size.width, $size.height);
+	});
+
+	const { renderStage, autoRender } = useThrelte();
+
+	// We need to disable auto rendering as soon as this component is
+	// mounted and restore the previous state when it is unmounted.
+	onMount(() => {
+		let before = autoRender.current;
+		autoRender.set(false);
+		return () => autoRender.set(before);
+	});
+
+	useTask(
+		(delta) => {
+			composer.render(delta);
+		},
+		{ stage: renderStage, autoInvalidate: false }
+	);
+</script>
