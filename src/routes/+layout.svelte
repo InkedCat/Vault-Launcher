b4a591f0a7866fcce62be96e6b0984beab6c7292
InkedCat <146424693+InkedCat@users.noreply.github.com>
2025-05-30 15:01:05 +0200

    feat(layout): implement automatic redirection to sign-in page for unauthenticated users

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,8 +6,18 @@
 	import '../app.css';
 	import { page } from '$app/state';
 
+	import { ModeWatcher } from 'mode-watcher';
 	let { data, children }: { data: LayoutData; children: Snippet } = $props();
+
+	let isConnected = $state(false);
+	$effect(() => {
+		if (!isConnected && window.location.pathname !== '/sign-in') {
+			window.location.href = '/sign-in';
+		}
+	});
 </script>
+
+<ModeWatcher />
 
 {@render children()}
 
